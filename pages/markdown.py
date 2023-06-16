12bacf9aa0a8ee68ab1fee1f0ea13437cc3e2d39
minjung00 <kmj931208@gmail.com>
2023-06-16 11:28:08 +0900

    마크다운

--- pages/markdown.py
+++ pages/markdown.py
@@ -1,28 +1,9 @@
-import streamlit as st # streamlit -> import (가져오기) -> as (st이름)
-# st라는 변수명으로 streamlit의 기능들을 사용하겠다.
-
-# st. -> ctrl + space -> 다양한 기능(함수, 메소드)를 가지고 있다
-
-st.title("나의 파이썬 웹 페이지")
-st.header("수업 8일차에 만들었어요!")
-st.subheader("그래도 잘 만들었죠?")
-st.write("내가 만든 페이지")
-st.video("https://www.youtube.com/watch?v=SaCheA6Njc4") # 유튜브 링크
-st.image("https://cdn.pixabay.com/photo/2023/06/02/14/12/woman-8035772_1280.jpg")  # 인터넷 링크
-st.image("https://i.imgur.com/QzWz9JO.png")  # 인터넷 링크
-st.image("img/img2.png")  # 파일 경로 (app.py)
-st.image(image="img/img2.png")  # 키워드를 사용해서...
-st.image("img/img2.png", use_column_width=True)  # 파일 경로 (app.py)
-st.image("img/img2.png", width=100)  # 파일 경로 (app.py)
-# https://imgur.com/
-
-#streamlit run app.py
-
-#마크다운
+import streamlit as st
+# 마크다운
 
 # st.write / st.markdown
 # st.write -> 입력하는 것에 맞춰서 알아서 결정 -> 마크다운
-
+st.title("마크다운")
 # 제목 마크다운
 st.write("""
 # 가장 큰 제목 텍스트 (h1 - headline1 -st.title)
@@ -47,9 +28,8 @@ text = """
 
 형광펜 : <mark>형광펜</mark>
 """
-#st.write(text)
+# st.write(text)
 st.markdown(text, unsafe_allow_html=True)
-
 
 # 레이아웃
 st.subheader("레이아웃")
@@ -110,18 +90,18 @@ st.divider()
 st.subheader("인용")
 st.write(f"""
     > 무언가
-    
+
     > 여러줄
-    
+
     책이나, 사람말 인용할 ㅁ대...
     >> 인용문 안에 인용
-    
+
     #### 코드
     `코드를 나타낼 때 주로 쓰이는 묶음 표시`
     ```
     여러 줄 코드
     ```
-    
+
     ```python
     print("파이썬")
     ```
